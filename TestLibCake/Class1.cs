850ddbd65eac8ced0e281543669a4835142f2ac7
Утин Артем Сергеевич <Utin.AS@partner.ru>
2017-12-19 12:29:58 +1000

    initial commit

---
+++ TestLibCake/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibCake
+{
+    public class Class1
+    {
+        public int Method(int v)
+        {
+            return v * v;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibCake;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var obj = new Class1();
+            Assert.AreEqual(16, obj.Method(4));
+            Assert.AreEqual(1, obj.Method(1));
+            Assert.AreEqual(25, obj.Method(-5));
+        }
+    }
+}
